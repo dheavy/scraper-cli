fcf2145b042cf8b8f887f6e933e181242b8b5a95
Davy Peter Braun <hello@davybraun.com>
2014-12-07 23:23:25 +0100

    Start laying out the CLI-bases scraper.

--- scraper.js
+++ scraper.js
@@ -1,15 +1,19 @@
 #!/usr/bin/env node
 
 var program = require('commander'),
+    config  = require('./lib/configuration'),
+    logger  = require('./lib/logger'),
     url, userId;
 
 program
   .version('0.3.0')
-  .option('-u, --url [url]', 'URL to crawl and scrape')
-  .option('-i, --id [id]', 'Job ID')
-  .option('-r, --requester [requester]','User ID')
+  .option('-u, --url [string]', 'url to crawl and scrape')
+  .option('-i, --id [int]', 'job id in queue')
+  .option('-r, --requester [int]','user id')
   .parse(process.argv);
 
 if (!program.url || !program.id || !program.requester) {
   return console.log('Error: missing argument(s)');
 }
+
+console.log(program.url, program.id, program.requester)
